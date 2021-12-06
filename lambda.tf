a43e84f612813c812a5edec85636e613d4e05b93
Scott Sickles <scottsickles@gmail.com>
2021-12-06 15:28:01 -0500

    make sure to set package type

--- lambda.tf
+++ lambda.tf
@@ -12,6 +12,7 @@ resource "aws_lambda_function" "this" {
   memory_size   = var.service_memory
   timeout       = var.service_timeout
   tags          = local.tags
+  package_type  = "Image"
   image_uri     = "${aws_ecr_repository.this.repository_url}:${local.app_version}"
 
   dynamic "vpc_config" {
