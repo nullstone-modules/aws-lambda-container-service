b01da708640a97d3da0a0cf5eefe699737197393
Brad Sickles <brad.sickles@gmail.com>
2022-06-16 10:54:02 -0400

    Add bootstrap image

---
+++ bootstrap_image.tf
@@ -0,0 +1,16 @@
+data "aws_ecr_authorization_token" "temporary" {
+  registry_id = data.aws_caller_identity.this.account_id
+}
+
+provider "dockerless" {
+  registry_auth {
+    address  = data.aws_ecr_authorization_token.temporary.proxy_endpoint
+    username = data.aws_ecr_authorization_token.temporary.user_name
+    password = data.aws_ecr_authorization_token.temporary.password
+  }
+}
+
+resource "dockerless_remote_image" "bootstrap" {
+  source = "nullstone/lambda-bootstrap:latest"
+  target = "${aws_ecr_repository.this.repository_url}:bootstrap"
+}
--- lambda.tf
+++ lambda.tf
@@ -4,20 +4,18 @@ locals {
   })
   env_vars = { for k, v in merge(local.standard_env_vars, var.service_env_vars) : k => v }
 
-  service_runtime   = "provided"
-  desired_image_uri = "${aws_ecr_repository.this.repository_url}:${local.app_version}"
-  current_image_uri = local.app_version != "" ? local.desired_image_uri : crane_image_tarball.bootstrap.name
+  effective_image_uri = "${aws_ecr_repository.this.repository_url}:${coalesce(local.app_version, "bootstrap")}"
 }
 
 resource "aws_lambda_function" "this" {
   function_name = local.resource_name
   role          = aws_iam_role.executor.arn
-  runtime       = local.service_runtime
+  runtime       = "provided"
   memory_size   = var.service_memory
   timeout       = var.service_timeout
   tags          = local.tags
   package_type  = "Image"
-  image_uri     = local.current_image_uri
+  image_uri     = local.effective_image_uri
 
   vpc_config {
     security_group_ids = [aws_security_group.this.id]
