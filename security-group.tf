ecd2ab1680621fc9ccf0a320860f9e2ba33959a1
Brad Sickles <brad.sickles@gmail.com>
2022-06-20 11:02:49 -0400

    consistent security groups

--- security-group.tf
+++ security-group.tf
@@ -1,10 +1,12 @@
 resource "aws_security_group" "this" {
-  name   = local.resource_name
-  vpc_id = local.vpc_id
-  tags   = merge(local.tags, { Name = local.resource_name })
+  name        = local.resource_name
+  vpc_id      = local.vpc_id
+  tags        = merge(local.tags, { Name = local.resource_name })
+  description = "Managed by Terraform"
 }
 
 resource "aws_security_group_rule" "this-https-to-world" {
+  description       = "Allow service to communicate with world over HTTPS"
   security_group_id = aws_security_group.this.id
   type              = "egress"
   protocol          = "tcp"
