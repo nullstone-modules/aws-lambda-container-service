200dc2cff300bda47063d426a713cdb2da1fdd57
Brad Sickles <brad.sickles@gmail.com>
2022-09-14 14:57:18 -0400

    Added secrets support with service_secrets var

--- role.tf
+++ role.tf
@@ -25,3 +25,38 @@ resource "aws_iam_role_policy_attachment" "executor_vpc" {
   role       = aws_iam_role.executor.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
+
+resource "aws_iam_role_policy" "executor" {
+  role   = aws_iam_role.executor.id
+  policy = data.aws_iam_policy_document.executor.json
+}
+
+locals {
+  // These are used to generate an IAM policy statement to allow the app to read the secrets
+  secret_arns                = [for as in aws_secretsmanager_secret.app_secret : as.arn]
+  secret_statement_resources = length(local.secret_arns) > 0 ? [local.secret_arns] : []
+}
+
+data "aws_iam_policy_document" "executor" {
+  statement {
+    sid       = "AllowBasicSecretsListing"
+    effect    = "Allow"
+    actions   = ["secretsmanager:ListSecrets"]
+    resources = ["*"]
+  }
+
+  dynamic "statement" {
+    for_each = local.secret_statement_resources
+
+    content {
+      sid       = "AllowReadSecrets"
+      effect    = "Allow"
+      resources = statement.value
+
+      actions = [
+        "secretsmanager:GetSecretValue",
+        "kms:Decrypt"
+      ]
+    }
+  }
+}
