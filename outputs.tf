632297a0229926a42971e069da1cf2136be483b0
Brad Sickles <brad.sickles@gmail.com>
2024-01-15 12:45:49 -0500

    Added metrics outputs

--- outputs.tf
+++ outputs.tf
@@ -41,6 +41,16 @@ output "log_reader" {
   sensitive   = true
 }
 
+output "metrics_reader" {
+  value       = module.logs.reader
+  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to read metrics from Cloudwatch."
+  sensitive   = true
+}
+
+output "metrics_mappings" {
+  value = local.metrics_mappings
+}
+
 output "artifact_source" {
   value       = "docker"
   description = "string ||| This module pulls its source as a docker image from ECR."
